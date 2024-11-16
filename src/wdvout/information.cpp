9eccc7a86dc6a03750380b38bf13a07ae6b97c88
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-16 15:29:13 +0000

    Update information.cpp

--- src/wdvout/information.cpp
+++ src/wdvout/information.cpp
@@ -1,7 +1,7 @@
 void displayInfo() {
 	std::cout << R"(
 
-WEBP Data Vehicle (wdvout v1.0). 
+WEBP Data Vehicle (wdvout v1.1). 
 Created by Nicholas Cleasby (@CleasbyCode) 13/11/2024.
 
 wdvout is a CLI tool for extracting hidden data from a wdvin "file-embedded" WEBP image. 
