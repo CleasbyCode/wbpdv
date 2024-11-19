13fb54538b81f6d890de18532ee9e9b656765200
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-19 17:41:13 +0000

    Add files via upload

--- src/wbpin/encryptFile.cpp
+++ src/wbpin/encryptFile.cpp
@@ -5,17 +5,17 @@ uint32_t encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& Fi
 	std::uniform_int_distribution<unsigned short> dis(1, 255); 
 	
 	constexpr uint8_t 
+		DEFAULT_PIN_INDEX = 0x95,
+		DEFAULT_PIN_XOR_INDEX = 0x90,
 		XOR_KEY_LENGTH = 234,
-		PIN_LENGTH = 4;
+		PIN_LENGTH = 9;
 
 	uint16_t 
-		data_file_start_index = 0x37C,
+		// data_file_start_index = 0x37C,
 		profile_data_size = 854,
-		cover_index = 0x35A,
 		deflate_file_size_index = 0x36F,
-		data_filename_index = cover_index + 1,
+		data_filename_index = 0x35B,
 		xor_key_index = 0x270,
-		pin_index = data_file_start_index,
 		encrypt_xor_pos = xor_key_index,
 		index_xor_pos = encrypt_xor_pos;
 
@@ -26,8 +26,10 @@ uint32_t encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& Fi
 	uint8_t 
 		iccp_chunk_size_field_index = 0x25,
 		profile_size_field_index = 0x26,
-		data_filename_length = Profile_Vec[cover_index],
+		data_filename_length = Profile_Vec[data_filename_index - 1],
 		xor_key[XOR_KEY_LENGTH],
+		pin_index =  DEFAULT_PIN_INDEX,
+		pin_xor_index = DEFAULT_PIN_XOR_INDEX,
 		value_bit_length = 32,
 		xor_key_length = XOR_KEY_LENGTH,
 		xor_key_pos = 0,
@@ -40,12 +42,13 @@ uint32_t encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& Fi
 		// Insert an extra byte in to the (profile) chunk to make it even.
 		// Update some variables to reflect this increase.
 		// Set flag in profile to indicate chunk size was odd and an extra byte was added.	
-		constexpr uint16_t EXTRA_BYTE_FLAG_INDEX = 0x373;
-		Profile_Vec.insert(Profile_Vec.begin() + pin_index, 0x19);
-		++pin_index;
-		++data_file_start_index;
+		constexpr uint16_t 
+			EXTRA_BYTE_FLAG_INDEX = 0x373,
+			DATA_FILE_START_INDEX = 0x37C;
+
+		Profile_Vec.insert(Profile_Vec.begin() + DATA_FILE_START_INDEX, 0x19); 
 		++iccp_chunksize;
-		Profile_Vec[EXTRA_BYTE_FLAG_INDEX] = 0x01;
+		Profile_Vec[EXTRA_BYTE_FLAG_INDEX] = true;
 	}
 	
 	// Write chunk size to the ICCP chunk size field
@@ -73,18 +76,17 @@ uint32_t encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& Fi
 		Profile_Vec.emplace_back(File_Vec[index_pos++] ^ xor_key[xor_key_pos++ % XOR_KEY_LENGTH]);
 	}
 	
+	xor_key_index = 0x270;
+
+	const uint32_t PIN = crcUpdate(&Profile_Vec[xor_key_index], XOR_KEY_LENGTH);
+	valueUpdater(Profile_Vec, pin_index, PIN, value_bit_length, true);
+
 	while(xor_key_length--) {
-		Profile_Vec[encrypt_xor_pos++] = Profile_Vec[index_xor_pos++] ^ Profile_Vec[pin_index++];
-		pin_index = pin_index >= PIN_LENGTH + data_file_start_index ? data_file_start_index : pin_index;
+		Profile_Vec[encrypt_xor_pos++] = Profile_Vec[index_xor_pos++] ^ Profile_Vec[pin_xor_index++];
+		pin_xor_index = pin_xor_index >= PIN_LENGTH + DEFAULT_PIN_XOR_INDEX ? DEFAULT_PIN_XOR_INDEX : pin_xor_index;
 	}
 	
-	pin_index = data_file_start_index;
-
-	const uint32_t 
-		PIN = getByteValue(Profile_Vec, pin_index),
-		COVER_PIN = getByteValue(Profile_Vec, cover_index);
-	
-	valueUpdater(Profile_Vec, pin_index, COVER_PIN, value_bit_length, true);
+	valueUpdater(Profile_Vec, pin_index, 0, value_bit_length, true);
 	
 	return PIN;
 }
--- src/wbpin/wbpin.h
+++ src/wbpin/wbpin.h
@@ -11,11 +11,12 @@
 #include <vector>
 #include <iterator>
 
-#include <zlib.h>
-#include <webp/decode.h>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+#include <C:\Users\Nick\source\zlib-1.3.1\decode.h>
 
 #include "profilesVec.cpp"
 #include "valueUpdater.cpp"
+#include "crc32.cpp"
 #include "writeFile.cpp"
 #include "searchFunc.cpp"
 #include "getByteValue.cpp"
@@ -28,6 +29,7 @@ template <uint8_t N>
 uint32_t searchFunc(std::vector<uint8_t>&, uint32_t, const uint8_t, const uint8_t (&)[N]);
 
 uint32_t 
+	crcUpdate(uint8_t*, uint32_t),
 	encryptFile(std::vector<uint8_t>&, std::vector<uint8_t>&, uint32_t, std::string&),
 	deflateFile(std::vector<uint8_t>),
 	getByteValue(const std::vector<uint8_t>&, const uint32_t);
@@ -35,7 +37,7 @@ uint32_t
 bool writeFile(std::vector<uint8_t>&);
 
 void
-	valueUpdater(std::vector<uint8_t>&, uint32_t, const uint32_t, uint8_t, bool),
+	valueUpdater(std::vector<uint8_t>&, uint32_t, const uint32_t, uint8_t),
 	displayInfo();
 
 int wbpIn(const std::string&, std::string&);
